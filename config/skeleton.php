fa0567dffbede55809211bdf0ea9e78fddd85898
Selçuk Çukur <5716652+selcukcukur@users.noreply.github.com>
2026-01-09 21:29:33 +0300

    skeleton(config): introduce base config file with default key

---
+++ config/skeleton.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | ℹ️ Package (Config)
+    |--------------------------------------------------------------------------
+    |
+    | Lorem ipsum dolor sit amet, consectetur adipiscing elit.  
+    | Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.  
+    | Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris. 
+    |
+    */
+    'key' => env('SKELETON_KEY', 'default')
+];
